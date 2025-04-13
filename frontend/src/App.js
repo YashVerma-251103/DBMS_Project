9ef0137408f35330bf4be7c9286e0fb252796eac
Unique <unique23568@iiitd.ac.in>
2025-04-13 18:30:41 +0530

    chnaged directories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,6 +56,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import SearchFlights from './pages/SearchFlights';
+import LoginSignUp from "./components/LoginSignUp";
+import AdminHome from "./components/AdminHome";
+import ManagerHome from "./components/ManagerHome";
+import EmployeeHome from "./components/EmployeeHome";
+import CustomerHome from "./components/CustomerHome";
 
 function App() {
   return (
@@ -68,6 +73,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/flights/search" element={<SearchFlights />} />
+          <Route path="/LoginSignup" element={<LoginSignUp />} />
+          <Route path="/AdminHome" element={<AdminHome />} />
+          <Route path="/ManagerHome" element={<ManagerHome />} />
+          <Route path="/EmployeeHome" element={<EmployeeHome />} />
+          <Route path="/CustomerHome" element={<CustomerHome />} />
         </Routes>
       </div>
     </Router>
